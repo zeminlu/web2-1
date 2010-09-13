550043303521b2d5b5ffd6e9b1c9aa0237476f60
angel.jardi <angel.jardi@b368f5ca-e7ec-bfde-7d26-00b13b979343>
2010-09-13 15:18:19 +0000

    Filtro para el manejo de errores.

---
+++ src/main/java/controller/InvalidHeaderException.java
@@ -0,0 +1,10 @@
+package controller;
+
+@SuppressWarnings("serial")
+public class InvalidHeaderException extends RuntimeException {
+
+	public InvalidHeaderException(){
+		super();
+	}
+	
+}
---
+++ src/main/java/controller/NotFoundException.java
@@ -0,0 +1,9 @@
+package controller;
+
+@SuppressWarnings("serial")
+public class NotFoundException extends RuntimeException {
+	
+		public NotFoundException(){
+			super();
+		}
+}
